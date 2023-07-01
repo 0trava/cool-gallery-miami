17c109b31cc2083f26b196d50b199677218dc738
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-07-01 12:11:01 +0300

    add transform style for slider

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
 const sliderMain = new Swiper('.slider_main', {
     freeMode: true,
     centeredSlides: true,
-    slidesPerView: 3,
+    mousewhell: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 2.5,
+            spaceBetween: 20,
+
+        },
+        680: {
+            slidesPerView: 3.5,
+            spaceBetween: 60, 
+        }
+    }
 
 })
