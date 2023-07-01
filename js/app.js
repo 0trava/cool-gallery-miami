1e9bb21d42a74efa832a89826fffad4610da8c51
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-07-01 19:07:14 +0300

    add text animate

--- js/app.js
+++ js/app.js
@@ -27,4 +27,10 @@ document.querySelectorAll('.slider__item').forEach(item => {
     item.addEventListener('click', event => {
         item.classList.toggle('opened')
     })
+});
+
+
+let desc = document.querySelector('.description')
+sliderMain.on('slideChange', e => {
+	sliderMain.activeIndex > 0 ? desc.classList.add('hidden') : desc.classList.remove('hidden')
 })
